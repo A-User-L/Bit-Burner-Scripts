863ee34493af2adebe9d1c0128c1f9fa07f6a526
A-User-L <notamonky@gmail.com>
2025-02-24 16:59:34 -0500

    Update funny:3.js

--- funny:3.js
+++ funny:3.js
@@ -3,32 +3,22 @@ export async function main(ns) {
   var name = ns.getHostname();
   var targetMaxMoney = ns.getServerMaxMoney(name);
 
+  while (true) {
+    var hackchance = ns.hackAnalyzeChance(name);
+    var availableTargetMoney = ns.getServerMoneyAvailable(name);
 
-  // if script is trying to run on home or hacknet server 0-10 exit
-  if (name == "home") {
-    ns.tprint("Can't Hack " + name + ". Exiting script");
-    await ns.sleep(5000);
-    ns.exit();
-    // if can hack
-  } else {
-    while (true) {
-      var hackchance = ns.hackAnalyzeChance(name);
-      var availableTargetMoney = ns.getServerMoneyAvailable(name);
+    // weaken if chance to hack chance is to small
+    if (hackchance < 0.70) {
+      await ns.weaken(name);
+      await ns.sleep(10);
 
-      // weaken if chance to hack chance is to small
-      if (hackchance < 0.70) {
-        await ns.weaken(name);
-        await ns.sleep(100);
-
-        // if hack chance isnt to small grow intill have enough money then hack
-      } else if ((hackchance >= 0.70) & (availableTargetMoney <= (targetMaxMoney * .70))) {
-        await ns.grow(name);
-        await ns.sleep(100);
-      } else {
-        await ns.hack(name);
-        await ns.sleep(100);
-      }
+      // if hack chance isnt to small grow intill have enough money then hack
+    } else if ((hackchance >= 0.70) & (availableTargetMoney <= (targetMaxMoney * .70))) {
+      await ns.grow(name);
+      await ns.sleep(10);
+    } else {
+      await ns.hack(name);
+      await ns.sleep(10);
     }
   }
-
 }
