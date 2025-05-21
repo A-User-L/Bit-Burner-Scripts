dfee907243ea84bfb6f424294b99cce44413391e
A-User-L <notamonky@gmail.com>
2025-05-20 23:18:57 -0400

    Update startingLoop.js
    Fix - If last script was running and you reran this script... it would go past the array "programList"

--- startingLoop.js
+++ startingLoop.js
@@ -12,55 +12,53 @@ export async function main(ns) {
   for(let a = 0; a < programList.length; a++) {
 
     // see if YOU have the file
-    if(ns.fileExists(programList[a], host)) {
-
-      // see if YOU can run the program
-      if((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) >= ns.getScriptRam(programList[a])) {
-
-        // is program running?
-        if(ns.isRunning(programList[a]) == false) {
-
-          // for "gang:3.js" check if in gang
-          if(programList[a] == "gang:3.js" && ns.gang.inGang() == false) {
-            ns.tprint("WARN: You are not in a gang...");
-            ns.tprint("Skiping...");
-            a++
-          }
-
-          // run program
-          ns.tprint(`Running ${programList[a]}`);
-          ns.run(programList[a]);
-
-          // give program time to run if verbose through terminal
-          for (let b = 0; b < termBose.length; b++) {
-            if(programList[a] == termBose[b]) {
-              do {
-                await ns.sleep(2000);
-              } while(ns.isRunning(programList[a]));
-            }
-            else {
-              await ns.sleep(1000);
-            }
-          }
-        }
-        else{
-          ns.tprint(`${programList[a]} is already running...`);
-          ns.tprint("Skiping...");
-        }
-      }
-      else {
-        ns.tprint(`WARN: You do not have enough RAM to run ${programList[a]}`);
-        ns.tprint("Skiping...");
-      }
-    }
-    else {
+    if(ns.fileExists(programList[a], host) == false) {
       ns.tprint(`ERROR: Cant Find ${programList[a]}...`);
       ns.tprint("Skiping...");
+      a++
+    }
+
+    // see if YOU can run the program
+    else if((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) <= ns.getScriptRam(programList[a])) {
+      ns.tprint(`WARN: You do not have enough RAM to run ${programList[a]}`);
+      ns.tprint("Skiping...");
+      a++
+    }
+
+    // for "gang:3.js" check if in gang
+    else if(programList[a] == "gang:3.js" && ns.gang.inGang() == false) {
+      ns.tprint("WARN: You are not in a gang...");
+      ns.tprint("Skiping...");
+      a++
+    }
+
+    // is program running?
+    else if(ns.isRunning(programList[a])) {
+      ns.tprint(`${programList[a]} is already running...`);
+      ns.tprint("Skiping...");
+    }
+    else{
+
+      // run program
+      ns.tprint(`Running ${programList[a]}`);
+      ns.run(programList[a]);
+    }
+
+    // give program time to run if verbose through terminal
+    for(let b = 0; b < termBose.length; b++) {
+      if(programList[a] == termBose[b]) {
+        do {
+          await ns.sleep(2000);
+        } while(ns.isRunning(programList[a]));
+      }
+      else {
+        await ns.sleep(1000);
+      }
     }
   }
 
   // does "backdoorTarget.js" exist?
-  if(ns.fileExists("backdoorTarget.js")){
+  if(ns.fileExists("backdoorTarget.js", host)){
     
     // faction list
     let factionServName = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "fulcrumassets"]
@@ -78,14 +76,15 @@ export async function main(ns) {
       let myLvL = ns.getHackingLevel();
 
       // wait if can't backdoor
-      do {
+      while(myLvL < backdoorNeed){
         await ns.sleep(1000);
-      } while(myLvL <= backdoorNeed)
+      };
 
       // backdoor target
       ns.run("backdoorTarget.js", 1, factionServName[i]);
     }
   }
-
+  ns.tprint("Script complete, exiting...");
+}
   ns.tprint("Script complete, exiting...");
 }
